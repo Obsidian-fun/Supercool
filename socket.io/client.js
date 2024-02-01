ece9b973916296d31c09c9687d993cff9d7134eb
Obsidian-fun <obsidiancrystal@tutanota.com>
2024-02-01 12:12:45 +0530

    Chatroom Design Changes

---
+++ socket.io/client.js
@@ -0,0 +1,46 @@
+// This is the client side code
+
+// const username = window.prompt('Enter your username?');
+//localStorage.setItem('username',username);
+//const user = localStorage.getItem('username');
+
+const input = document.querySelector('#message-input');
+const form = document.querySelector('#form');
+const message = document.querySelector('#message');
+
+// Socket on connection,
+socket.on('connection', (msg) =>{
+  display(msg);
+});
+
+// For sending messages,
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+    if (input.value) {
+        socket.emit('chat message', input.value);
+        input.value = '';
+    }
+});
+
+// For receiveing messages,
+socket.on('chat message', (msg) => {
+  display(msg);
+  window.scrollTo(0, document.querySelector('#message-container').scrollHeight);
+});
+
+socket.on('disconnect' , (msg)=>{
+  const el=document.createElement('li');
+  el.innerText = `See ya later amigo 👋`;
+  document.querySelector('ul').append(el);
+});
+
+function display(message) {
+  const el = document.createElement('li');
+  el.innerText = `${message}`;
+  document.querySelector('ul').append(el);
+}
+
+// check out differences between textContent, innerHTML and innerText at https://builtin.com/software-engineering-perspectives/innerhtml-vs-innertext
+        
+
+// https://sparrowshare.com/app/listings/02eb6d47-0d42-4924-b552-5b0c3adb3d09
