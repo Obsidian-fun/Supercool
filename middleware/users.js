111ef59440b6df5443765d2465f684ae23a1c1a5
Obsidian-fun <obsidiancrystal@tutanota.com>
2024-03-01 23:58:59 +0530

    modified:   middleware/users.js 	modified:   register.html

--- middleware/users.js
+++ middleware/users.js
@@ -1,6 +1,7 @@
 
 export function validateRegister(req,res,next){
   if (!req.body.username || req.body.username.length < 3){
+    console.log(req.body.username, req.body.username.length);
     return res.status(400).send({
       message:'Username should be greater than 3 characters',
     });
