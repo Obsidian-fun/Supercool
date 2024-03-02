9036af2148056b2f20e3810fa1c9403eca80b0a2
Obsidian-fun <obsidiancrystal@tutanota.com>
2024-03-03 00:33:13 +0530

    modified:   middleware/users.js 	deleted:    socket.io/commit-list.txt

--- middleware/users.js
+++ middleware/users.js
@@ -6,7 +6,7 @@ export function validateRegister(req,res,next){
       message:'Username should be greater than 3 characters',
     });
   }
-  if (!req.body.password || req.body.password.length <= 8){
+  if (!req.body.password || req.body.password.length < 8){
     return res.status(400).send({
       message:'Password must be atleast 8 characters',
     });
